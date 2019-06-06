e20c4948a8ff7a8ad209a65a1954c805f9ae86df
oyerajnish123 <rajnishparashar111@gmail.com>
2019-06-06 15:24:50 +0530

    commmt

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -6,33 +6,50 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ListView list;
-    String data[]={"Suman","Hariom","Kanhaiya","laxaman","Raushan","Rajnish","Tathastu","Amit","Shubham","Suman","Hariom","Kanhaiya","laxaman","Raushan","Rajnish","Tathastu","Amit","Shubham"};
 
-
+    Spinner list;
+    String data[] = {"Rajnish", "Amit", "Chanda", "Sanju", "Arvind", "Anjali", "Shubham", "Raushan","Rajnish", "Amit", "Chanda", "Sanju", "Arvind", "Anjali", "Shubham", "Raushan"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list=(ListView)findViewById(R.id.Listview);
 
-        ArrayAdapter adapter=new ArrayAdapter(MainActivity.this,
-                android.R.layout.simple_list_item_1,data);
+        list = (Spinner)findViewById(R.id.listview);
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,data);
         list.setAdapter(adapter);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                TextView txt=(TextView)view;
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // TextView txt = (TextView)view;
 
-                Toast.makeText(MainActivity.this,txt.getText().toString(),
-                        Toast.LENGTH_SHORT).show();
+                // Toast.makeText(MainActivity.this, txt.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                switch (position){
+                    case 0:
+                        Toast.makeText(MainActivity.this, "First", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "Second", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "Third", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
 
             }
         });
+
     }
 }
